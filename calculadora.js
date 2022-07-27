3eb1c0508833f405d448cbec4633d168a34615a3
Bruno Oliveira <brunogabriel2608@gmail.com>
2022-07-27 16:56:40 -0300

    <FEAT>(Criaçao do documento javascript e incorporaçao da funcionalidade de adicionar notas)

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+var soma = 0;
+var vezes = 0;
+
+function adicionar(){
+    
+     if (document.body.querySelector("#nota").value === '' || document.body.querySelector("#nota").value > 10
+      || document.body.querySelector("#nota").value < 0){
+            
+        window.alert('Digite um valor valido');
+    
+    }else {
+        
+        vezes = vezes + 1;
+        let nota = document.body.querySelector("#nota").value;
+        let lista = document.body.querySelector('#lista').innerHTML;
+        lista = lista+"<li>a nota "+vezes+" foi "+nota+"</li>";
+        document.body.querySelector('#lista').innerHTML = lista;
+        soma = soma + Number(nota);
+        media = soma/(vezes);
+        document.body.querySelector("span").innerText = media;
+    
+    }
+
+}
